ce590fc6b0180e4837360d6ad886f612f5e62cd0
Firdavs Kholikov <lokpi96@gmail.com>
2025-04-04 14:16:45 +0500

    .

--- cmd/main.go
+++ cmd/main.go
@@ -19,7 +19,25 @@ func main() {
 		fmt.Println("Error:", err)
 		return
 	}
-	RAMUsage = RAMUsage / 1024 / 1024
-	RAMTotal = RAMTotal / 1024 / 1024
-	fmt.Println("RAM Usage:", RAMUsage, "MB", "Total:", RAMTotal, "MB")
+	RAMUsageMB := RAMUsage / 1024 / 1024
+	RAMTotalMB := RAMTotal / 1024 / 1024
+	fmt.Println("RAM Usage:", RAMUsageMB, "MB", "Total:", RAMTotalMB, "MB")
+
+	DiskUsed, DiskTotal, err := metrics.DiskUsage()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	DiskUsedGb := DiskUsed / 1024 / 1024 / 1024
+	DiskTotalGb := DiskTotal / 1024 / 1024 / 1024
+	fmt.Println("Disk Used:", DiskUsedGb, "Gb", "Total:", DiskTotalGb, "Gb")
+
+	NetSent, NetRecv, err := metrics.NetUsage()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	NetSentKb := NetSent / 1024
+	NetRecvKb := NetRecv / 1024
+	fmt.Println("Net Sent:", NetSentKb, "Kb", "Received:", NetRecvKb, "Kb")
 }
--- internal/metrics/collector.go
+++ internal/metrics/collector.go
@@ -4,7 +4,9 @@ import (
 	"time"
 
 	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/disk"
 	"github.com/shirou/gopsutil/v3/mem"
+	"github.com/shirou/gopsutil/v3/net"
 )
 
 func CPUPerc() (float64, error) {
@@ -22,4 +24,17 @@ func RAMUsage() (uint64, uint64, error) {
 	return v.Used, v.Total, nil
 }
 
-//func DiskUsage() (float64, error) {
+func DiskUsage() (uint64, uint64, error) {
+	v, err := disk.Usage("/")
+	if err != nil {
+		return 0, 0, err
+	}
+	return v.Used, v.Total, nil
+}
+func NetUsage() (uint64, uint64, error) {
+	v, err := net.IOCounters(false)
+	if err != nil {
+		return 0, 0, err
+	}
+	return v[0].BytesSent, v[0].BytesRecv, nil
+}
