63e627d9cb4e081a9fc8f9b610c4d953c7c33063
Firdavs Kholikov <lokpi96@gmail.com>
2025-04-04 16:04:42 +0500

    .

--- cmd/main.go
+++ cmd/main.go
@@ -7,13 +7,26 @@ import (
 )
 
 func main() {
-	cpuUsage, err := metrics.CPUPerc()
+	Collect, err := metrics.CollectMetrics()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Printf("CPU Usage: %.2f%%\n", Collect.CPUUsage)
+	fmt.Printf("RAM Usage: %d MB\n", Collect.RAMUsed)
+	fmt.Printf("RAM Total: %d MB\n", Collect.RAMTotal)
+	fmt.Printf("Disk Used: %d GB\n", Collect.DiskUsed)
+	fmt.Printf("Disk Total: %d GB\n", Collect.DiskTotal)
+	fmt.Printf("Net Sent: %d KB\n", Collect.NetSent)
+	fmt.Printf("Net Received: %d KB\n", Collect.NetRecv)
+
+	/*cpuUsage, err := metrics.CPUPerc()
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 	fmt.Printf("CPU Usage: %.2f%%\n", cpuUsage)
-
+	//
 	RAMUsage, RAMTotal, err := metrics.RAMUsage()
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -22,7 +35,7 @@ func main() {
 	RAMUsageMB := RAMUsage / 1024 / 1024
 	RAMTotalMB := RAMTotal / 1024 / 1024
 	fmt.Println("RAM Usage:", RAMUsageMB, "MB", "Total:", RAMTotalMB, "MB")
-
+	//
 	DiskUsed, DiskTotal, err := metrics.DiskUsage()
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -31,7 +44,7 @@ func main() {
 	DiskUsedGb := DiskUsed / 1024 / 1024 / 1024
 	DiskTotalGb := DiskTotal / 1024 / 1024 / 1024
 	fmt.Println("Disk Used:", DiskUsedGb, "Gb", "Total:", DiskTotalGb, "Gb")
-
+	//
 	NetSent, NetRecv, err := metrics.NetUsage()
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -39,5 +52,5 @@ func main() {
 	}
 	NetSentKb := NetSent / 1024
 	NetRecvKb := NetRecv / 1024
-	fmt.Println("Net Sent:", NetSentKb, "Kb", "Received:", NetRecvKb, "Kb")
+	fmt.Println("Net Sent:", NetSentKb, "Kb", "Received:", NetRecvKb, "Kb")*/
 }
--- internal/metrics/collector.go
+++ internal/metrics/collector.go
@@ -9,6 +9,16 @@ import (
 	"github.com/shirou/gopsutil/v3/net"
 )
 
+type MetricsData struct {
+	CPUUsage  float64 `json:"cpu_usage"`
+	RAMUsed   uint64  `json:"ram_used_mb"`
+	RAMTotal  uint64  `json:"ram_total_mb"`
+	DiskUsed  uint64  `json:"disk_used_gb"`
+	DiskTotal uint64  `json:"disk_total_gb"`
+	NetSent   uint64  `json:"net_sent_kb"`
+	NetRecv   uint64  `json:"net_recv_kb"`
+}
+
 func CPUPerc() (float64, error) {
 	percentages, err := cpu.Percent(time.Second, false)
 	if err != nil {
@@ -37,4 +47,31 @@ func NetUsage() (uint64, uint64, error) {
 		return 0, 0, err
 	}
 	return v[0].BytesSent, v[0].BytesRecv, nil
+}
+func CollectMetrics() (MetricsData, error) {
+	cpuUsage, err := CPUPerc()
+	if err != nil {
+		return MetricsData{}, err
+	}
+	ramUsage, ramTotal, err := RAMUsage()
+	if err != nil {
+		return MetricsData{}, err
+	}
+	diskUsed, diskTotal, err := DiskUsage()
+	if err != nil {
+		return MetricsData{}, err
+	}
+	netSent, netRecv, err := NetUsage()
+	if err != nil {
+		return MetricsData{}, err
+	}
+	return MetricsData{
+		CPUUsage:  cpuUsage,
+		RAMUsed:   ramUsage / 1024 / 1024,
+		RAMTotal:  ramTotal / 1024 / 1024,
+		DiskUsed:  diskUsed / 1024 / 1024 / 1024,
+		DiskTotal: diskTotal / 1024 / 1024 / 1024,
+		NetSent:   netSent / 1024,
+		NetRecv:   netRecv / 1024,
+	}, nil
 }
